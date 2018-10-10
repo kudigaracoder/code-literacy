f23b3cfe612425cf7e76d54d73631a1e6a489080
Ravi Shanker <ravishanker@outlook.com>
2018-10-10 13:53:05 -0400

    EXIF logic added;Not working yet

--- week5/sketch.js
+++ week5/sketch.js
@@ -3,14 +3,24 @@ var xPos = yPos = 0;
 var imgHeight = imgWidth = 250;
 var imgPadding = 10;
 
+var cv;
+
 function preload() {
     for (var i=1; i<=5; i++)
     {
         img[i] = loadImage("./images/image-"+i+".jpg");
     }
 }
+
+function centerCanvas() {
+    var canvasWidth = (windowWidth - width) / 2;
+    var canvasHeight = (windowHeight - height) / 2;
+    cv.position(canvasWidth, canvasHeight);
+}
+
 function setup() {
-    createCanvas(windowWidth,windowHeight);
+    cv = createCanvas(1290,500);
+    centerCanvas();
     noLoop();
 }
 
@@ -20,9 +30,18 @@ function draw() {
         image(img[i], xPos, yPos, imgWidth, imgHeight);
         xPos = xPos + imgWidth + imgPadding;
     }
+}
 
+function mousePressed() {
+    for (var i=1; i<=5; i++)
+    {
+        EXIF.getData(img[i], function() {
+            var allMetaData = EXIF.getAllTags(this);
+            console.log(allMetaData);
+        });
+    }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  centerCanvas();
 }
